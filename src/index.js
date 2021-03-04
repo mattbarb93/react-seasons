c6791064a511b18b060fde54136d45c30e28280e
Matheus Barbosa <mattbarb93@gmail.com>
2021-03-04 15:05:23 -0500

    starting to add state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
-const App = () => {
-    return
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {lat: null };
+    }
+
+
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+           (error) => console.log(error) 
+        );
+
+        return <div>Latitude: </div>
+    }
 }
+
+ReactDOM.render(<App />, document.querySelector('#root'));
